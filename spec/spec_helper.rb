628f050aee9121d271bad73ee1005e6acc36b5a1
ksearfos <ksearfos@covermymeds.com>
2014-10-16 16:14:19 -0400

    minor tweaks to existing test suite, and update Fetching class (and tests) to allow for unsecured option

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -9,5 +9,6 @@ RSpec.configure do |config|
   # order dependency and want to debug it, you can fix the order by providing
   # the seed, which is printed after each run.
   #     --seed 1234
-  config.order = "random"
+  # config.order = "random"
+  config.color = :enabled
 end
